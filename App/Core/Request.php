64cc1edf79326ad3def14554ba5d614dc8e0b5d7
Amir Ranjbar <amircoder1400@gmail.com>
2024-08-22 19:04:10 +0330

    Class-Request-added

--- App/Core/Request.php
+++ App/Core/Request.php
@@ -1,10 +1,52 @@
 <?php
+
 namespace App\Core;
 
-class Request{
+use JetBrains\PhpStorm\NoReturn;
+
+class Request
+{
+
+    private array $params;
+    private mixed $method;
+    private mixed $ip;
+    private mixed $agent;
 
     public function __construct()
     {
-        echo "request class";
+        $this->params = $_REQUEST;
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->ip = $_SERVER['REMOTE_ADDR'];
+        $this->agent = $_SERVER['HTTP_USER_AGENT'];
+
     }
+
+    public function getParams($key)
+    {
+        return $this->params[$key] ?? null;
+    }
+
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    public function getAgent()
+    {
+        return $this->agent;
+    }
+
+    #[NoReturn] public function redirect($route): void
+    {
+        header("Location: " . site_url($route));
+        die();
+    }
+
+
 }
--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
 # front controller
+use App\Core\Request;
 use App\Core\StupidRouter;
 use App\Utilities\Url;
 
 include "bootstrap/init.php";
 
+$request = new Request();
+echo $request->getParams('name');
 
-echo Url::current_route() . "<hr>";
+if($request->getParams('name') === 'ali') {
+    $request->redirect('colors/red');
+}
+//echo "<pre>";
+//var_dump($request);
+//echo  "<hr>";
+//echo $request->getMethod() . "<hr>";
+//echo $request->getIp() . "<hr>";
+//echo $request->getAgent() . "<hr>";
 
 
 $router = new StupidRouter();
